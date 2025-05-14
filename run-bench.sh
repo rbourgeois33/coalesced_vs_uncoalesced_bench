91b66e089cee84a8024513948e2748ed55cef051
rb263871 <remi.bourgeois@cea.fr>
2025-05-14 09:08:59 +0200

    fix

--- run-bench.sh
+++ run-bench.sh
@@ -4,14 +4,13 @@ rm *.txt
 
 # Step 1: Run ./main --scale_test and capture the integer output
 #./main --scale_test
-#X=$?
+X=28
 
 echo "Scale test returned: $X"
 
 # Step 2: Loop from 0 to X and run ./main with parameters
 for ((i=0; i<=X; i++)); do
-#  ./main --pow_size $X --N_Repeat 20 --pow_max_dist "$i"
-  ./main --pow_size 28 --N_Repeat 20 --pow_max_dist "$i"
+  ./main --pow_size $X --N_Repeat 20 --pow_max_dist "$i"
 done
 
 python ../plot.py *.txt
